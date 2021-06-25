87fe3a5f587a21cbd535b89080eaf3041f7a7dce
Hieu.vuminh <hieu.vuminh@pixta.co.jp>
2021-06-25 13:37:34 +0700

    login back-end

--- app/controllers/infor_controller.rb
+++ app/controllers/infor_controller.rb
@@ -1,4 +1,8 @@
 class InforController < ApplicationController
   def infor_get
+    user = session[:user]
+    if user == nil || user == ""
+      redirect_to login_path
+    end
   end
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -6,7 +6,16 @@ class LoginController < ApplicationController
   def login_post
     user = params[:user]
     pass = params[:pass]
-    a = Account.where()
-    redirect_to infor_path
+    @a = Account.where(:username =>  user, :password => pass).first
+    if @a == nil
+      flash[:notice] = "Account doesn't exist"
+      redirect_to login_path
+    elsif @a.status_id == 2
+      flash[:notice] = "Your account was inactive by admin"
+      redirect_to login_path
+    else
+      session[:user] = @a.username
+      redirect_to infor_path
+    end
   end
 end
