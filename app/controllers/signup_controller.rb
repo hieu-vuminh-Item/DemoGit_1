3ddc026215b8998d5df7f5e85339e61686cfc501
Hieu.vuminh <hieu.vuminh@pixta.co.jp>
2021-06-25 13:48:05 +0700

    sighnup back-end

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,18 @@
 class SignupController < ApplicationController
   def signup_get
   end
+
+  def signup_post
+    user = params[:user]
+    pass = params[:pass]
+    a = Account.where(:username => user).first
+    if a != nil
+      flash[:notice] = "Username has already used"
+      redirect_to signup_path
+    else
+      Account.create(username: user, password: pass, role_id: 2, status_id: 1)
+      flash[:notice] = "Signup sucessful"
+      redirect_to login_path
+    end
+  end
 end
