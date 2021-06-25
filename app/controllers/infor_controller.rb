cb2918fcebb4b777a46422968ec9e722ef0b4bd1
Hieu.vuminh <hieu.vuminh@pixta.co.jp>
2021-06-25 15:12:07 +0700

    infor, logout back-end

--- app/controllers/infor_controller.rb
+++ app/controllers/infor_controller.rb
@@ -3,6 +3,16 @@ class InforController < ApplicationController
     user = session[:user]
     if user == nil || user == ""
       redirect_to login_path
+    else
+      @a = Account.where(:username => user).first
+      if @a.role_id == 1
+        @listacc = Account.all
+      end
     end
+  end
+
+  def logout
+    session[:user] = nil
+    redirect_to login_path
   end
 end
